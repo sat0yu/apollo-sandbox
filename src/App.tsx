dc38bca31380cbeff4cf5ceca83469efea7b271a
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2018-08-02 21:11:39 +0900

    apollo integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import ApolloClient from "apollo-boost";
 import * as React from 'react';
-import './App.css';
+import { ApolloProvider } from "react-apollo";
+import { ExchangeRates } from "./Query";
 
+import './App.css';
 import logo from './logo.svg';
+
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h2>My first Apollo app 🚀</h2>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        <ExchangeRates />
+        </div>
+      </ApolloProvider>
     );
   }
 }
